2a23b4f366ff96459427dea69fc683d853a240e4
Chiro-art <129538159+Chiro-art@users.noreply.github.com>
2023-07-07 13:46:41 +0700

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Chiro | Gallery</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="/">Home</a></li>
+        <li><a href="/post">Post</a></li>
+        <li><a href="gallery">Gallery</a></li>
+        <li style="float:right"><a href="/about">About</a></li>
+    </ul>
+
+<div style="padding:20px;margin-top:0px;background-color:#FFF8DE;height:1500px;" >
+
+    <h1> Your Best Stock Image and Video Provider </h1>
+
+<h1 style="float:right;padding: 80px;"> Scroll Down to See My Info</h1>
+     
+
+</div>
+
+<footer class="footer">
+  <p>Chiro Dogo </p>
+<p> </p>
+<p> </p>
+  <a style="color:white;"href="mailto:Chiro123@gmail.com">Chiro123@gmail.com</a></p>
+<div class="footer-links">
+    <a href="https://www.instagram.com/" ><img src="img/instagram1.png" alt="instagram" width="20" height="20"></a>
+    <a href="https://www.youtube.com/"><img src="img/youtube.png" alt="youtube" width="20" height="20"></a>
+</div>
+  
+</footer>
+
+</body>
+</html>
